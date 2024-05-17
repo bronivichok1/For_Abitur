245ec407393e1d38fe5732c153c1f4deef91b63c
bronivichok1 <lazarenkov01@gmail.com>
2024-05-17 14:16:13 +0300

    dev 0.6.2

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,18 @@
 import ButtonForNavigate from '../components/ButtonForNavigate'
+import { useTranslation } from 'react-i18next';
 
  function Home() {
- 
+  const { t, i18n } = useTranslation()
   return ( 
     <div >
       <main>
+        <div align="right"className="row">
+            <button className="btn-3 btn-sep icon-send" value="ru" onClick={(event) => { event.preventDefault(); i18n.changeLanguage('ru'); }}>RU</button>
+            <button className="btn-11 btn-sep icon-send" value="en" onClick={(event) => { event.preventDefault(); i18n.changeLanguage('en'); }}>EN</button>
+        </div>
         <div align="center">
-          <ButtonForNavigate title='Заполнить заявление абитуриента' adress='FillData' style="btn btn-1 btn-sep icon-info"/>
-          <ButtonForNavigate title='Исправить/удалить заявление абитуриента' adress='FindData' style="btn btn-4 btn-sep icon-send"/>
+          <ButtonForNavigate title={t('But1')} adress='FillData' style="btn btn-1 btn-sep icon-info"/>
+          <ButtonForNavigate title={t('But2')} adress='FindData' style="btn btn-4 btn-sep icon-send"/>
         </div>
       </main>
     </div>
